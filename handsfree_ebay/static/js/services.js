5f31e4bfc4267fbd7edc5e7d10924f2b264fa8af
Bizuwork Melesse <bmelesse@elifsis.com>
2018-03-03 15:39:37 -0800

    Fix audio to binary file loading

--- handsfree_ebay/static/js/services.js
+++ handsfree_ebay/static/js/services.js
@@ -1,67 +1,43 @@
 
-console.log(Cookies.get('csrftoken'));
-//  function imageTopMargin() {
 
-// 	var pageHeight = window.innerHeight;
-// 	var topMargin = pageHeight * 0.2 + "px";
-// 	document.getElementById("hal-image-ID").style["margin-top"] =  topMargin;
-// 	console.log(topMargin);
-// }
+function transmitAudio(audioBlobObj) {
 
-// function transmitAudio(audioURL) {
+    var csrftoken = Cookies.get('csrftoken');
+    console.log(csrftoken);
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.open("POST", "audio-post", true);
+	xmlhttp.setRequestHeader("X-CSRFToken", csrftoken);
+	xmlhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+	xmlhttp.send(audioBlobObj);
+}
 
+function voiceAssit() {
+	// request permission to access audio stream
+	navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
+	    // store streaming data chunks in array
+	    const chunks = [];
+	    // create media recorder instance to initialize recording
+	    const recorder = new MediaRecorder(stream);
+	    // function to be called when data is received
+	    recorder.ondataavailable = e => {
+	      // add stream data to chunks
+	      chunks.push(e.data);
+	      // if recorder is 'inactive' then recording has finished
+	      if (recorder.state == 'inactive') {
+	          // convert stream data chunks to a 'webm' audio format as a blob
+	          const blob = new Blob(chunks, { type: 'audio/x-wav' });
+	          // convert blob to URL so it can be assigned to a audio src attribute
+	          transmitAudio(blob);
+	      }
+	    };
+	    // start recording with 1 second time between receiving 'ondataavailable' events
+	    recorder.start(1000);
+	    // setTimeout to stop recording after 15 seconds
+	    setTimeout(() => {
+	        // this will trigger one final 'ondataavailable' event and set recorder state to 'inactive'
+	        recorder.stop();
+	    }, 5000);
+	  }).catch(console.error);
 
-//     // const downloadEl = document.createElement('a');
-//     // downloadEl.style = 'display: block';
-//     // downloadEl.innerHTML = 'download';
-//     // downloadEl.download = 'audio.wav';
-//     // downloadEl.href = blobUrl;
-//     // const audioEl = document.createElement('audio');
-//     // audioEl.controls = true;
-//     // const sourceEl = document.createElement('source');
-//     // sourceEl.src = blobUrl;
-//     // sourceEl.type = 'audio/wav';
-//     // audioEl.appendChild(sourceEl);
-//     // document.body.appendChild(audioEl);
-//     // document.body.appendChild(downloadEl);
-//     var qs = "audioURL="+audioURL;
-//     console.log("audio url: " + audioURL);
-//     var csrftoken = Cookies.get('csrftoken');
-//     console.log(csrftoken);
-// 	// var xmlhttp = new XMLHttpRequest();
-// 	// xmlhttp.open("POST", "audio-post", false);
-// 	// xmlhttp.setRequestHeader("X-CSRFToken", csrftoken);
-// 	// xmlhttp.setRequestHeader('Content-Type', 'audio/x-wav');
-// 	// xmlhttp.send(qs);
-// }
-
-// function voiceAssit() {
-// 	// request permission to access audio stream
-// 	navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
-// 	    // store streaming data chunks in array
-// 	    const chunks = [];
-// 	    // create media recorder instance to initialize recording
-// 	    const recorder = new MediaRecorder(stream);
-// 	    // function to be called when data is received
-// 	    recorder.ondataavailable = e => {
-// 	      // add stream data to chunks
-// 	      chunks.push(e.data);
-// 	      // if recorder is 'inactive' then recording has finished
-// 	      if (recorder.state == 'inactive') {
-// 	          // convert stream data chunks to a 'webm' audio format as a blob
-// 	          const blob = new Blob(chunks, { type: 'audio/x-wav' });
-// 	          // convert blob to URL so it can be assigned to a audio src attribute
-// 	          transmitAudio(URL.createObjectURL(blob));
-// 	      }
-// 	    };
-// 	    // start recording with 1 second time between receiving 'ondataavailable' events
-// 	    recorder.start(1000);
-// 	    // setTimeout to stop recording after 15 seconds
-// 	    setTimeout(() => {
-// 	        // this will trigger one final 'ondataavailable' event and set recorder state to 'inactive'
-// 	        recorder.stop();
-// 	    }, 1000);
-// 	  }).catch(console.error);
-
-// }
+}
 // window.onload = imageTopMargin();
--- handsfree_ebay/views.py
+++ handsfree_ebay/views.py
@@ -3,19 +3,30 @@ from __future__ import unicode_literals
 from django.http import HttpResponseForbidden
 from django.shortcuts import render
 
+import requests
+
 
 def index_view(request):
 	return render(request, 'index.html', {})
 
 def audio_post_view(request):
 	# prohibit get requests at this url/view
-	# if request.method == "GET":
-	# 	return HttpResponseForbidden()
+	if request.method == "GET":
+		return HttpResponseForbidden()
 
-	print "beginning of post--------------------"
-	print request.POST
-	print "end of post--------------------" 
+	# print "beginning of post--------------------"
+	uploadedFile = open("sample.wav", "wb") 
+	# uploadedFile.write(request.body);
+	# uploadedFile.close()
 
+	# print request.POST
+	# print request.FILES
+	# # response = requests.get(request.POST.get("audioURL"))
+	# # print "the response: ", request.content
+	# print "request body: ", request.body
+
+	# print "end of post--------------------" 
+	return render(request, 'index.html', {})
 
 
 
